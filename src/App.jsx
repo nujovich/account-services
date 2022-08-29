464fbaae95f89fa9ce1db7ad2430de437d2b6b89
nadiauj <nadiauj@ext.inditex.com>
2022-08-29 18:37:06 -0300

    Add about us section

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,33 @@ function App() {
     <section className="banner-section">
       <div className="banner-container">
         <h2>Soluciones Contables</h2>
-        <span>por Marcos Markocic y Agustín Rodríguez Otero</span>
+        <p>por Marcos Markocic y Agustín Rodríguez Otero</p>
+      </div>
+    </section>
+    <section className="about-us-section">
+      <div className="about-us-container">
+        <h2 className="about-us-title">¿Quienes Somos?</h2>
+        <p className="about-us-description">Somos un equipo con dinamismo y plena dedicación al cliente, abordando la actividad del mismo en su totalidad, brindando un servicio que va más allá de la liquidación impositiva</p>
+        <h2 className="about-us-title">¿Cuál es nuestra misión?</h2>
+        <p className="about-us-description">Ser una opción en cada problemática diaria, convirtiéndonos en un eslabón que suma a la perspectiva y al fortalecimiento diario del cliente.</p>
+        <h2 className="about-us-title">¿Cuál es nuestro valor agregado?</h2>
+        <p className="about-us-description">Aquel conformado por plena predisposición, dedicación y responsabilidad. Creemos en la importancia de la comunicación, y para ello brindamos canales personalizados para que nuestros clientes siempre tengan una rápida respuesta ante alguna necesidad</p>
+      </div>
+      <div className="profile-container">
+        <div className="profile-element">
+          <div className="image-container">
+            <img className="profile-image" src="src/assets/images/marcos-fake.jpg" alt="Marcos"></img>
+          </div>
+          <h3>Marcos Alberto Markocic</h3>
+            <p>Contador Público, con especialidad en Costos y Gestión Empresarial, Rosarino, de 32 años, cursó su carrera de grado y posgrado en la Universidad Nacional de Rosario.</p>
+        </div>
+        <div className="profile-element">
+        <div className="image-container">
+            <img className="profile-image" src="src/assets/images/agustin-fake.jpg" alt="Agustín"></img>
+          </div>
+          <h3>Agustín Rodríguez Otero</h3>
+          <p>Contador Público, con especialidad en Costos y Gestión Empresarial, Rosarino, de 32 años, cursó su carrera de grado y posgrado en la Universidad Nacional de Rosario.</p>
+        </div>
       </div>
     </section>
     </div>
