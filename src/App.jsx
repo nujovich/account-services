13ef242b783fe4829fef30f2c7517b571cfe22f1
nadiauj <nadiauj@ext.inditex.com>
2022-09-12 21:59:03 -0300

    change of services

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,18 @@ function App() {
         <iconify-icon icon="eva:menu-outline" width="40" height="40"></iconify-icon>
       </label>
       <nav className="menu">
-        <a href="#services-section">Servicios</a>
-        <a href="#">Clientes</a>
-        <a href="#">Contacto</a>
+        <ul className="menu-items">
+          <li>
+            <a href="#services-section" className="menu-link">Servicios</a>
+          </li>
+          <li>
+            <a href="#" className="menu-link">Clientes</a>
+          </li>
+          <li>
+            <a href="#" className="menu-link">Contacto</a>
+          </li>
+        </ul>
+        
         <label htmlFor="check-button" className="cross-icon">
           <iconify-icon icon="codicon:chrome-close" width="40" height="40"></iconify-icon>
         </label>
@@ -40,14 +49,14 @@ function App() {
       <div className="profile-container">
         <div className="profile-element">
           <div className="image-container">
-            <img className="profile-image" src="src/assets/images/marcos-fake.jpg" alt="Marcos"></img>
+            <img className="profile-image" src="/assets/images/marcos-fake.jpg" alt="Marcos"></img>
           </div>
           <h3>Marcos Alberto Markocic</h3>
             <p>Contador Público, con especialidad en Costos y Gestión Empresarial, Rosarino, de 32 años, cursó su carrera de grado y posgrado en la Universidad Nacional de Rosario.</p>
         </div>
         <div className="profile-element">
         <div className="image-container">
-            <img className="profile-image" src="src/assets/images/agustin-fake.jpg" alt="Agustín"></img>
+            <img className="profile-image" src="/assets/images/agustin-fake.jpg" alt="Agustín"></img>
         </div>
           <h3>Agustín Rodríguez Otero</h3>
           <p>Contador Público, con especialidad en Costos y Gestión Empresarial, Rosarino, de 32 años, cursó su carrera de grado y posgrado en la Universidad Nacional de Rosario.</p>
@@ -61,22 +70,64 @@ function App() {
           <div className="icon-container">
             <iconify-icon icon="ic:baseline-account-balance-wallet" width="60" height="60"></iconify-icon>
           </div>
-          <h3>Presentación de balances</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum repellendus alias magnam assumenda officia libero corrupti sapiente deserunt iure delectus!</p>
+          <h3>Presentaciones impositivas</h3>
+          <ul>
+            <li>Impuestos municipales, provinciales y nacionales</li>
+            <li>Regularización de Pasivos impositivos y Planes de pago</li>
+          </ul>
         </div>
         <div className="services-element">
           <div className="icon-container">
             <iconify-icon icon="ic:baseline-account-balance" width="60" height="60"></iconify-icon>
           </div>
-          <h3>Cierre de ganancias</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum repellendus alias magnam assumenda officia libero corrupti sapiente deserunt iure delectus!</p>
+          <h3>Certificaciones personales para Organismos de Control</h3>
+          <ul>
+            <li>Certificaciones UIF Origen de fondos</li>
+            <li>Manifestación de Bienes</li>
+          </ul>
         </div>
         <div className="services-element">
           <div className="icon-container">
             <iconify-icon icon="ic:baseline-switch-account" width="60" height="60"></iconify-icon>
           </div>
-          <h3>Liquidación de sueldos</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum repellendus alias magnam assumenda officia libero corrupti sapiente deserunt iure delectus!</p>
+          <h3>Balances contables para Sociedades Comerciales</h3>
+          <p>Confección de documentos oficiales para avalar el estado financiero de tu empresa a nivel patrimonial y económico</p>
+        </div>
+        <div className="services-element">
+        <div className="icon-container">
+            <iconify-icon icon="fluent:receipt-add-24-filled" width="60" height="60"></iconify-icon>
+          </div>
+          <h3>Contabilidad Laboral</h3>
+          <ul>
+            <li>F931</li>
+            <li>Liquidación de recibos de sueldo</li>
+          </ul>
+      </div>
+      <div className="services-element">
+        <div className="icon-container">
+            <iconify-icon icon="map:courthouse" width="60" height="60"></iconify-icon>
+          </div>
+          <h3>Asesoramiento Contable-Judicial</h3>
+          <ul>
+            <li>Peritos contables de Parte</li>
+          </ul>
+      </div>
+      <div className="services-element">
+        <div className="icon-container">
+            <iconify-icon icon="ic:baseline-switch-account" width="60" height="60"></iconify-icon>
+          </div>
+          <h3>Especialización en Sindicatura Concursal</h3>
+          <p>Concurso Preventivo y Quiebra</p>
+      </div>
+      <div className="services-element">
+        <div className="icon-container">
+            <iconify-icon icon="carbon:calculator-check" width="60" height="60"></iconify-icon>
+          </div>
+          <h3>Especialización en Costos</h3>
+          <ul>
+            <li>Análisis de costos</li>
+            <li>Gestión de información para la toma de decisiones</li>
+          </ul>
         </div>
       </div>
     </section>
